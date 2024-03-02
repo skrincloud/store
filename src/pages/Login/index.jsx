4c4bc6988ed89c8870f11584add88ee708792e87
luis <luisbarboz.17@gmail.com>
2024-03-02 15:58:25 -0500

    TDA-2 Login form implemented

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,55 +1,72 @@
-import { useState } from 'react';
-import { client } from '../../server';
-import { readMe } from '@directus/sdk';
+import { readMe } from "@directus/sdk";
+import { useState } from "react";
+import Field from "../../components/Field/index.jsx";
+import { client } from "../../server";
+
+import "./login.css";
 
 function Login(props) {
   const { setUser } = props;
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   async function onLogin(event) {
     event.preventDefault();
 
     try {
-      await client.login(email, password)
-      const user = await client.request(readMe())
+      await client.login(email, password);
+      const user = await client.request(readMe());
       setUser(user);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   function buildOnChange(setter) {
     return (event) => {
       setter(event?.target?.value);
-    }
+    };
   }
 
   return (
-    <div>
-      <form>
-        <div>
-          <label>Correo</label>
-          <input
-            type="text"
-            name="username"
-            onChange={buildOnChange(setEmail)}
-          />
-        </div>
+    <div className="Login">
+      <div>
+        <p className="Login-title">Iniciar sesión</p>
+        <p className="Login-subtitle">
+          Por favor, inicie sesión para continuar
+        </p>
+      </div>
 
-        <div>
-          <label>Contraseña</label>
-          <input
-            type="password"
-            name="password"
-            onChange={buildOnChange(setPassword)}
-          />
-        </div>
+      <form className="Login-form">
+        <Field
+          name="email"
+          value={email}
+          onChange={buildOnChange(setEmail)}
+          label="Correo"
+          type="email"
+        />
+        <Field
+          name="password"
+          value={password}
+          onChange={buildOnChange(setPassword)}
+          label="Contraseña"
+          type="password"
+        />
 
-        <button type="submit" onClick={onLogin}>Ingresar</button>
+        <button type="submit" onClick={onLogin} className="Login-form-button">
+          Ingresar
+        </button>
       </form>
+
+      <p className="Login-form-forgot" href="">
+        Olvidé mi contraseña
+      </p>
+
+      <p className="Login-form-register">
+        ¿No tienes cuenta?&nbsp;<span>Contáctanos</span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
