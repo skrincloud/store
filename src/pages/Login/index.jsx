cf26f57286b9c4b8263147cfb7414b35bc5ef229
Marcelo Velásquez <marceloo.velasquez@hotmail.com>
2024-03-03 12:47:06 -0500

    Add expanded layout for the login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import { readMe } from '@directus/sdk'
 import { useState } from 'react'
 import { client } from '../../api'
 import Field from '../../components/Field'
+import ExpandedLayout from '../../layouts/ExpandedLayout'
 
 import './Login.css'
 
@@ -29,45 +30,47 @@ function Login(props) {
   }
 
   return (
-    <div className="Login">
-      <div>
-        <h2 className="Login-title">Iniciar sesión</h2>
-        <p className="Login-subtitle">
-          Por favor, inicie sesión para continuar
+    <ExpandedLayout centered>
+      <div className="Login">
+        <div>
+          <h2 className="Login-title">Iniciar sesión</h2>
+          <p className="Login-subtitle">
+            Por favor, inicie sesión para continuar
+          </p>
+        </div>
+
+        <form className="Login-form">
+          <Field
+            name="email"
+            value={email}
+            type="email"
+            icon="email"
+            label="Correo"
+            onChange={buildOnChange(setEmail)}
+          />
+          <Field
+            name="password"
+            value={password}
+            type="password"
+            icon="lock"
+            label="Contraseña"
+            onChange={buildOnChange(setPassword)}
+          />
+
+          <button type="submit" onClick={onLogin} className="Login-form-button">
+            Ingresar
+          </button>
+        </form>
+
+        <p className="Login-form-forgot" href="">
+          Olvidé mi contraseña
+        </p>
+
+        <p className="Login-form-register">
+          ¿No tienes cuenta?&nbsp;<span>Contáctanos</span>
         </p>
       </div>
-
-      <form className="Login-form">
-        <Field
-          name="email"
-          value={email}
-          type="email"
-          icon="email"
-          label="Correo"
-          onChange={buildOnChange(setEmail)}
-        />
-        <Field
-          name="password"
-          value={password}
-          type="password"
-          icon="lock"
-          label="Contraseña"
-          onChange={buildOnChange(setPassword)}
-        />
-
-        <button type="submit" onClick={onLogin} className="Login-form-button">
-          Ingresar
-        </button>
-      </form>
-
-      <p className="Login-form-forgot" href="">
-        Olvidé mi contraseña
-      </p>
-
-      <p className="Login-form-register">
-        ¿No tienes cuenta?&nbsp;<span>Contáctanos</span>
-      </p>
-    </div>
+    </ExpandedLayout>
   )
 }
 
