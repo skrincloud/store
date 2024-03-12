4e87fe5de2ac662f6e5e116c1af68c811dfeea14
luis <luisbarboz.17@gmail.com>
2024-03-11 22:59:43 -0500

    Pass props to the elements componentnet header

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,7 @@ const url = 'https://store.itmarck.com'
 export const client = createDirectus(url, { globals: { fetch } })
   .with(authentication('json', { storage }))
   .with(rest())
+
+export function buildImageUrl(imageId) {
+  return `${url}/assets/${imageId}`
+}
