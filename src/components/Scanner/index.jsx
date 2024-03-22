f7668e05d12f4b30c31b2fe01fb5d0ccf31be23a
Javcho <javier120699lili@gmail.com>
2024-03-21 23:45:38 -0500

    Added dependencies for HTML5QRcodeScanner

--- src/components/Scanner/index.jsx
+++ src/components/Scanner/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import Icon from '../../components/Icon'
+import Html5QrcodePlugin from "./Html5QrcodePlugin"
 import './Scanner.css'
 
 function Scanner() {
+  const [barDecoded, setBarDecoded] = useState('initialState')
+
+  const onNewScanResult = (decodedText) => {
+    setBarDecoded(decodedText)
+  };
+
   return (
     <section className="Scanner">
       <Icon name="photo_camera" color="hint"></Icon>
       <p className="Scanner__text">Toca para activar el escaner</p>
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+      { barDecoded }
     </section>
   )
 }
