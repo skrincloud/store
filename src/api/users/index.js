6d5519ddd733962646dfe15a522d5638df2e4ed3
luis <luisbarboz.17@gmail.com>
2024-03-31 16:03:18 -0500

    Get user with data store

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,3 +4,18 @@ import { client } from '..'
 export async function getAuthenticatedUser() {
   return await client.request(readMe())
 }
+
+export async function getAuthenticatedUserWithStore() {
+  const result = await client.request(
+    readMe({
+      fields: [
+        '*',
+        {
+          store: ['*'],
+        },
+      ],
+    }),
+  )
+
+  return result
+}
