493e23e31d0888fe839c6dcf41a32f3e157d3a5f
Javcho <javier120699lili@gmail.com>
2024-04-15 00:26:13 -0500

    Added Quagga library and logic to active camera

--- src/components/Scanner/index.jsx
+++ src/components/Scanner/index.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import Html5QrcodePlugin from "./Html5QrcodePlugin"
+import { useEffect, useState } from 'react';
+import Icon from '../../components/Icon'
 import './Scanner.css'
+import Quagga from 'quagga'; // ES6
 
 function Scanner() {
   const [barDecoded, setBarDecoded] = useState('initialState')
@@ -9,12 +10,56 @@ function Scanner() {
     setBarDecoded(decodedText)
     console.log(barDecoded)
   };
+  const [isScannerActive, setIsScannerActive] = useState(false)
+
+  const startScanner = () => {
+    setIsScannerActive(true)
+
+  }
+
+  useEffect(() => {
+    if (!isScannerActive) return
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#scanner-source'),
+        constraints: {
+          width: { min: 640 },
+          height: { min: 480 },
+          facingMode: "environment",
+          aspectRatio: { min: 1, max: 2 }
+        }
+      },
+      locator: {
+        patchSize: "medium",
+        halfSample: true
+      },
+      numOfWorkers: 2,
+      frequency: 10,
+      locate: true,
+      decoder: {
+        readers: ["code_128_reader"]
+      }
+    }, function (err) {
+      if (err) {
+        console.log(err);
+        return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+    });
+  }, [isScannerActive]);
 
   return (
     <section className="Scanner">
-      <Html5QrcodePlugin
-          qrCodeSuccessCallback={onNewScanResult}
-        />
+      {(!isScannerActive) ?
+        <div onClick={startScanner}>
+          <Icon name="photo_camera" color="hint"></Icon>
+          <p className="Scanner__text">Toca para activar el escaner</p>
+        </div> :
+        <div id='scanner-source' className='Scanner__source'></div>
+      }
     </section>
   )
 }
