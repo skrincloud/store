1c3d923fc669af4f4bf964028ecfa7e47471bfe5
Javcho <javier120699lili@gmail.com>
2024-05-05 14:42:04 -0500

    Fixed list devices error

--- src/components/Scanner/Quagga.js
+++ src/components/Scanner/Quagga.js
@@ -7,14 +7,14 @@ export function setOnRead(onRead) {
 export async function startQuagga() {
   await navigator.mediaDevices
     .getUserMedia({ video: true })
-  const videoDevices = await Quagga.CameraAccess.enumerateVideoDevices();
+  const deviceId = await getCameraId();
   Quagga.init({
     inputStream: {
       name: "Live",
       type: "LiveStream",
       target: document.querySelector('#scanner-source'),
       constraints: {
-        deviceId: videoDevices[videoDevices.length - 1].deviceId
+        deviceId
       }
     },
     locator: {
@@ -34,4 +34,10 @@ export async function startQuagga() {
     }
     Quagga.start();
   });
+}
+
+async function getCameraId() {
+  const videoDevices = await navigator.mediaDevices.enumerateDevices();
+  const filterDevices = videoDevices.filter(device => !!device.deviceId && device.kind === 'videoinput');
+  return filterDevices[filterDevices.length - 1].deviceId;
 }
