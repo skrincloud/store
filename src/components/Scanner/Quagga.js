8d3b54d4bba709da68562c2dea53e5556c0867d7
Javcho <javier120699lili@gmail.com>
2024-05-15 00:53:07 -0500

    Release tracks after found the camera to use

--- src/components/Scanner/Quagga.js
+++ src/components/Scanner/Quagga.js
@@ -5,41 +5,45 @@ export function setOnRead(onRead) {
 }
 
 export async function startQuagga() {
-  await navigator.mediaDevices
-    .getUserMedia({ video: true })
-  const camera = await getCamera();
-  Quagga.init({
-    inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      target: document.querySelector('#scanner-source'),
-      constraints: {
-        deviceId: camera.deviceId,
+  try {
+    const camera = await getCamera();
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#scanner-source'),
+        constraints: {
+          deviceId: camera.deviceId,
+        }
+      },
+      locator: {
+        patchSize: "medium",
+        halfSample: true
+      },
+      numOfWorkers: 2,
+      frequency: 10,
+      locate: true,
+      decoder: {
+        readers: ["ean_reader"]
       }
-    },
-    locator: {
-      patchSize: "medium",
-      halfSample: true
-    },
-    numOfWorkers: 2,
-    frequency: 10,
-    locate: true,
-    decoder: {
-      readers: ["ean_reader"]
-    }
-  }, function (err) {
-    if (err) {
-      alert(`Error al iniciar tu camara  ${camera.label} \n ${err}`);
-      console.log(err);
-      return
-    }
-    Quagga.start();
-  });
+    }, function (err) {
+      if (err) {
+        alert(`Error al iniciar tu camara  ${camera.label} \n ${err}`);
+        console.log(err);
+        return
+      }
+      Quagga.start();
+    });
+  } catch (error) {
+    console.log(error);
+    alert(`Error al iniciar tu camara \n ${error}`);
+  }
 }
 
 async function getCamera() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   const videoDevices = await navigator.mediaDevices.enumerateDevices();
   const filterDevices = videoDevices.filter(device => !!device.deviceId && device.kind === 'videoinput');
-  console.log(filterDevices);
+  stream.getTracks().forEach(track => track.stop());
   return filterDevices[filterDevices.length - 1];
 }
